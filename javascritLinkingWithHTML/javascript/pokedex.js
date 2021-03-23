98945417b0492d0b5e74d88e011284c10c4cdc70
userVinnieLopez <81201113+userVinnieLopez@users.noreply.github.com>
2021-03-22 21:31:51 -0500

    first commit

---
+++ javascritLinkingWithHTML/javascript/pokedex.js
@@ -0,0 +1,68 @@
+const pokedex = [
+ {
+    name: "unown",
+    url: "https://pokeapi.co/api/v2/pokemon/201/"
+  },
+  {
+    name: "wobbuffet",
+    url: "https://pokeapi.co/api/v2/pokemon/202/"
+  },
+  {
+    name: "girafarig",
+    url: "https://pokeapi.co/api/v2/pokemon/203/"
+  },
+  {
+    name: "pineco",
+    url: "https://pokeapi.co/api/v2/pokemon/204/"
+  },
+  {
+    name: "forretress",
+    url: "https://pokeapi.co/api/v2/pokemon/205/"
+  },
+  {
+    name: "dunsparce",
+    url: "https://pokeapi.co/api/v2/pokemon/206/"
+  },
+  {
+    name: "gligar",
+    url: "https://pokeapi.co/api/v2/pokemon/207/"
+  },
+  {
+    name: "steelix",
+    url: "https://pokeapi.co/api/v2/pokemon/208/"
+  },
+  {
+    name: "snubbull",
+    url: "https://pokeapi.co/api/v2/pokemon/209/"
+  },
+  {
+    name: "granbull",
+    url: "https://pokeapi.co/api/v2/pokemon/210/"
+  },
+  {
+    name: "qwilfish",
+    url: "https://pokeapi.co/api/v2/pokemon/211/"
+  },
+  {
+    name: "scizor",
+    url: "https://pokeapi.co/api/v2/pokemon/212/"
+  },
+  {
+    name: "shuckle",
+    url: "https://pokeapi.co/api/v2/pokemon/213/"
+  },
+  {
+    name: "heracross",
+    url: "https://pokeapi.co/api/v2/pokemon/214/"
+  },
+  {
+    name: "sneasel",
+    url: "https://pokeapi.co/api/v2/pokemon/215/"
+  },
+  {
+    name: "teddiursa",
+    url: "https://pokeapi.co/api/v2/pokemon/216/"
+  }]
+
+  console.log(pokedex[1].name);
+ 
---
+++ javascritLinkingWithHTML/javascript/variable.js
@@ -0,0 +1,24 @@
+
+
+
+const a = console.log(typeof {name: "josh", name: "melvin", name:"lory"})
+ console.log(typeof a);
+// console.log(a[0]); // with this a[0] I am trying to get the name josh
+// console.log(typeof a[2]); // and with this one I am trying to get lory so we can leave melvin out 
+
+["Joshua", "melvin", 'Lory' , a, 50, 25 ,1000] //this is an array it can hold any kind of javascrit information
+// {name: "josh", name: "melvin", name:"lory"} // this is an object
+
+var list = [
+    { date: '12/1/2011', reading: 3, id: 20055 },
+    { date: '13/1/2011', reading: 5, id: 20053 },
+    { date: '14/1/2011', reading: 6, id: 45652 }
+];
+console.log(list[2]);
+console.log(list[2].reading)
+
+const object1 = { date: '12/1/2011', reading: 3, id: 20055 };
+console.log(object1.id);
+console.log(object1.reading);
+
+
